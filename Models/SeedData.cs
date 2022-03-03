36c5e2a661457cf673990a2d858f957485efdefb
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2022-03-03 23:39:01 +0100

    1.1 Database renamed

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -22,6 +22,7 @@ namespace LibApp.Models
                 context.MembershipTypes.AddRange(
                     new MembershipType
                     {
+                        Name="AAA",
                         Id = 1,
                         SignUpFee = 0,
                         DurationInMonths = 0,
@@ -29,6 +30,7 @@ namespace LibApp.Models
                     },
                     new MembershipType
                     {
+                        Name="AAA",
                         Id = 2,
                         SignUpFee = 30,
                         DurationInMonths = 1,
@@ -36,6 +38,7 @@ namespace LibApp.Models
                     },
                     new MembershipType
                     {
+                        Name="AAA",
                         Id = 3,
                         SignUpFee = 90,
                         DurationInMonths = 3,
@@ -43,6 +46,7 @@ namespace LibApp.Models
                     },
                     new MembershipType
                     {
+                        Name="AAA",
                         Id = 4,
                         SignUpFee = 300,
                         DurationInMonths = 12,
