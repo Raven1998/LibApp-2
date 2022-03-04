5a7d45c05f358cbc67d5dabf2771e58ad4b7d660
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2022-03-04 23:27:47 +0100

    2 Book Validation(required data,number in stock range,validation from user's side,anti-foreign token)

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,25 +6,26 @@ using System.Threading.Tasks;
 
 namespace LibApp.Models
 {
-    public class Book
-    {
-        public int Id { get; set; }
-		[Required]
+	public class Book
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Please provide correct book name")]
 		[StringLength(255)]
 		public string Name { get; set; }
-
-		[Required]
+		[Required(ErrorMessage = "Please provide correct author name")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre must be specified")]
 		public Genre Genre { get; set; }
-		[Required]
-		[Display(Name = "Genre")]
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
-		[Display(Name="Realease Date")]
+		[Required(ErrorMessage = "Realease Date is requaired")]
+		[Display(Name = "Release Date")]
 		public DateTime ReleaseDate { get; set; }
+		[Required(ErrorMessage = "Number in Stock is requaired")]
+		[Display(Name = "Number in Stock")]
+		[Range(1, 20, ErrorMessage = "Number In Stock must be between 1 - 20")]
 		public int NumberInStock { get; set; }
 		public int NumberAvailable { get; set; }
 	}
-      
+
 }
