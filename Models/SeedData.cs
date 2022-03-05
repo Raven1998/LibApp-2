a34b6a9ebdfedb6979f4a6577584b8b779ed7449
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2022-03-05 18:36:03 +0100

    6 datatables

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using LibApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
